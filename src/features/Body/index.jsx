ec481878185c33f1411e3612f55f5da8e0b845b6
Agboola Tobi Samuel <tobi4tee@gmail.com>
2024-04-19 16:22:22 +0100

    first commit

---
+++ src/features/Body/index.jsx
@@ -0,0 +1,20 @@
+import Unlock from "./unlock";
+import Achievements from "./achievements";
+import Calender from "./calender";
+import Customer from "./customer";
+import CommunityUpdate from "./communityUpdate";
+
+
+const Body = ()=>{
+    return(
+        <>
+            <Unlock/>
+            <Achievements/>
+            <Calender/>
+            <Customer/>
+            <CommunityUpdate/>
+        </>
+    )
+}
+
+export default Body;
